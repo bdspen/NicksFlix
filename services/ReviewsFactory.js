e72b4b86c78c8972cc177261472d6539974b5885
Ben <bdspen@gmail.com>
2015-10-02 15:45:30 -0700

    added form to reviews.html, form work properly

--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -1,6 +1,6 @@
 nicksFlix.factory('ReviewsFactory', function ReviewsFactory() {
   var factory = {};
-  factory.reviews = [{ input: "I'm happy, hope you're happy too. Never look back, walk tall, act fine. Have you sought fortune evasive and shy? Beware the savage jaw of 1984.", title: "An All-Too-Serious Breakdown of 'Space Jam'", id: 1}];
+  factory.reviews = [];
 
   factory.addReview = function() {
 
