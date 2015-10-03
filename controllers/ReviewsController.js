910b16e864baf2c5894dfdef17a113a95bb5afe5
Ben <bdspen@gmail.com>
2015-10-02 21:51:30 -0700

    add utilitiesfactory,and edit reviewscontroller to have addreview function, error when trying to add a review

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,4 +1,12 @@
-nicksFlix.controller('ReviewsCtrl', function ReviewsCtrl($scope, ReviewsFactory) {
+nicksFlix.controller('ReviewsCtrl', function ReviewsCtrl($scope, ReviewsFactory, UtilitiesFactory, $stateParams) {
+  $scope.review = UtilitiesFactory.findById(ReviewsFactory.reviews, $stateParams.reviewId);
   $scope.reviews = ReviewsFactory.reviews;
   $scope.ReviewsFactory = ReviewsFactory;
+
+  $scope.addReview = function() {
+    $scope.reviews.push({ input: userReview, title: factory.userTitle,
+      id: reviews.length + 1});
+    $scope.userTitle = null;
+    $scope.userReview = null;
+  }
 });
--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -1,15 +1,5 @@
 nicksFlix.factory('ReviewsFactory', function ReviewsFactory() {
   var factory = {};
   factory.reviews = [];
-
-  factory.addReview = function() {
-    console.log(factory.reviews);
-
-    factory.reviews.push({ input: factory.userReview, title: factory.userTitle,
-      id: factory.reviews.length + 1});
-
-    factory.userReview = null;
-    factory.userTitle = null;
-  };
   return factory;
 });
